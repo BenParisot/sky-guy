4a190169287df6e665c7e5010bd71a31c226d947
Ben <me@benparisot.com>
2022-02-08 17:12:18 -0800

    adds border top and labels physics logic

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ const playerController = {
     left: false,
     right: false,
     up: false,
+    down: false,
 
     keyListener: function (event) {
         let key_state = (event.type === 'keydown') ? true: false;
@@ -31,6 +32,9 @@ const playerController = {
                 break;
             case 39:
                 playerController.right = key_state;
+                break;
+            case 40:
+                playerController.down = key_state;
                 break;
         }
     }
@@ -50,39 +54,65 @@ const loop = function () {
         player.xVelocity += 0.5;
         console.log('right pressed');
     }
+    // if (playerController.down) {
+    //     player.yVelocity -= 10;
+    //     player.jumping = true;
+    //     console.log('down pressed');
+    // }
 
-    player.yVelocity += 1.5;
+    //gravity
+    player.yVelocity += 1;
     player.x += player.xVelocity;
     player.y += player.yVelocity;
-    player.xVelocity *= 0.9;
-    player.yVelocity *= 0.9;
+    player.xVelocity *= .94;
+    player.yVelocity *= 1.2;
 
+    //bottom border?
     if (player.y > 386 - 16 -32) {
         player.jumping = false;
         player.y = 386 - 16 - 32;
         player.yVelocity = 0;
     }
 
+    //top border?
+    if (player.y < 20) {
+        player.jumping = false;
+        player.y = 20;
+        player.yVelocity = 0;
+    }
+
+    //window loop
     if (player.x < -20) {
         player.x = 1220;
     } else if (player.x > 1220) {
         player.x = -20;
     }
 
+    //background styles
     context.fillStyle = '#81b0c6';
     context.fillRect(0, 0, 1220, 400);
 
+    //player styles
     context.fillStyle = "orange";
     context.beginPath();
     context.rect(player.x, player.y, player.width, player.height);
     context.fill();
 
+    //border-bottom
     context.strokeStyle = "green";
     context.lineWidth = 30;
     context.beginPath();
     context.moveTo(0, 385);
     context.lineTo(1220, 385);
     context.stroke();
+
+     //border-top
+     context.strokeStyle = "green";
+     context.lineWidth = 30;
+     context.beginPath();
+     context.moveTo(0, 10);
+     context.lineTo(1220, 10);
+     context.stroke();
 
     window.requestAnimationFrame(loop);
 };
